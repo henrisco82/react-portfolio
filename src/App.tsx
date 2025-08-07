c7d103070656084a06e8e40518389e6f49043b83
Henry Odohi Unah <henry.unah@gmail.com>
2025-08-07 19:01:24 +0200

    update base name in browser router

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/react-portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
