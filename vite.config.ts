1b8977b8f3b4e076ccdd2065d13b0e11386729d3
Henry Odohi Unah <henry.unah@gmail.com>
2025-08-07 21:41:18 +0200

    fixing github 404 error page with index

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/react-portfolio/',
-})
+  base: '/react-portfolio/', // <-- make sure this is your GitHub repo name
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '', // this will copy it into dist/ as 404.html
+          rename: '404.html',
+        },
+      ],
+      watch: {
+        reloadPageOnChange: false,
+      },
+    }),
+  ],
+});
